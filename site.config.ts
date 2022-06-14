36eb73980ba2f5eebcd9bfe5823441edcf2b3b9a
Haoming <94905401+floating15@users.noreply.github.com>
2022-06-14 21:57:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,15 +50,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Notes',
+      pageId: 'Notes-036b59e356c3491d8d9593682048d683'
+    },
+    {
+      title: 'About',
+      pageId: 'About-d0d07d2113864a4295dc899474e638aa'
+    }
+  ]
 })

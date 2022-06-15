b9630897553fd2e69ac05fc4fe75aad6bcb76bfe
Haoming <94905401+floating15@users.noreply.github.com>
2022-06-15 14:10:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'https://twitter.com/push105501',
-  github: 'https://github.com/floating15',
-  linkedin: 'https://www.linkedin.com/in/haoming-wang-b0a164171/',
+  twitter: 'push105501',
+  github: 'floating15',
+  linkedin: 'haoming-wang-b0a164171/',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 

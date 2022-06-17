595be60ba06eb6d49adc651e442a238a3c71fbe2
Haoming <94905401+floating15@users.noreply.github.com>
2022-06-17 22:08:12 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -57,10 +57,6 @@ export default siteConfig({
       pageId: '036b59e356c3491d8d9593682048d683'
     },
     {
-      title: 'Posts1',
-      pageId: '32fee5c36a7843a2ba59cd9e96653fc2'
-    },
-    {
       title: 'About',
       pageId: 'd0d07d2113864a4295dc899474e638aa'
     }

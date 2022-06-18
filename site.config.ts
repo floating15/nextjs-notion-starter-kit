ce053b9400ccac6b07cee4ee7871c2804c7d8b6d
Haoming <94905401+floating15@users.noreply.github.com>
2022-06-18 17:32:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -48,8 +48,8 @@ export default siteConfig({
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-//   navigationStyle: 'default'
+  // important pages.
+//   navigationStyle: 'default' 
   navigationStyle: 'custom',
   navigationLinks: [
     {
